a712ead5eb0ea20734ac9e1ea72976be991843b6
Lubos Michalic <lnmichalic@gmail.com>
2017-05-02 12:23:23 +0100

    Written fourth test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(9)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
 end
