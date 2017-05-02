e9d4907992102739e20fa4d1fba9e1b5cc4d886b
bannastre <bannastre@gmail.com>
2017-05-02 12:32:18 +0100

    Test four passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,5 @@
 def fizzbuzz(number)
+  return "FizzBuzz" if number % 3 == 0 && number % 5 == 0
   return "fizz" if number % 3 == 0
-  "buzz" if number % 5 == 0
+  return "buzz" if number % 5 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,4 +13,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed a multiple of 5' do
     expect(fizzbuzz(10)).to eq 'buzz'
   end
+  it 'returns "FizzBuzz" when passed a multiple of both 3 & 5' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
 end
