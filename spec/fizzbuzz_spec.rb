7700beb8df5cee10809059f93acdb0cf353bbeca
bannastre <bannastre@gmail.com>
2017-05-02 12:15:17 +0100

    Written third test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
 end
